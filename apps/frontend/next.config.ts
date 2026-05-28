f7830ad030575257f1e0cd05b33c0a193c42a5a9
viet.pham <viet.pham@asnet.com.vn>
2026-05-28 15:03:08 +0700

    [#140] chore: Update Next.js configuration to set outputFileTracingRoot for turbopack

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -2,6 +2,7 @@ import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  outputFileTracingRoot: path.join(__dirname, "../.."),
   turbopack: {
     root: path.join(__dirname, '../..'),  },
 };
